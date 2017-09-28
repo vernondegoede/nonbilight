2a11f8aa2d82b80482b87aca2047b0f62b35bda8
Vernon de Goede <vernon@ecurring.nl>
2017-09-28 08:15:44 +0200

    Remove Hue listeners from main process

--- main/index.js
+++ main/index.js
@@ -1,20 +1,13 @@
 // Packages
 const { BrowserWindow, app } = require("electron");
 const prepareNext = require("electron-next");
-const {
-  setEventListeners: setHueEventListeners,
-  loadClientFromStorage,
-} = require("./utils/hue");
 const showMainWindow = require("./windows/main");
 
 // Prepare the renderer once the app is ready
 app.on("ready", async () => {
   await prepareNext("./renderer");
 
-  // showConnectWindow();
   showMainWindow();
-  setHueEventListeners();
-  loadClientFromStorage();
 });
 
 // Quit the app once all windows are closed
